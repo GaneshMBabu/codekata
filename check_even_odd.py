d85e219365d0053b65e51c23eacdec0f4b1a3b0f
GaneshMBabu <mganeshbabur@gmail.com>
2017-06-21 12:04:05 +0530

    Create check_even_odd.py

---
+++ check_even_odd.py
@@ -0,0 +1,8 @@
+n = int(input("Enter the number to check: "))
+def check(a):
+       num=a
+       if (num % 2 == 0):
+              print("even")
+       else :
+               print ("odd")
+check(n)
