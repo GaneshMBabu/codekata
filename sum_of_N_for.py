c62168e1adfa6140b540b77ef1e56b554c153648
GaneshMBabu <mganeshbabur@gmail.com>
2017-06-22 10:25:42 +0530

    Update sum_of_N_for.py

--- sum_of_N_for.py
+++ sum_of_N_for.py
@@ -1,6 +1,9 @@
 num = int(input("Enter the limit :"))
 a=0
-for i in range (0,num):
-       b=int(input("Enter the number to add "))
-       a=a+b
-print(a)
+if (num>0):
+       for i in range (0,num):
+              b=int(input("Enter the number to add "))
+              a=a+b
+       print(a)
+else:
+    print("Enter a valid option")
