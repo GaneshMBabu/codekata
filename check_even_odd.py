c73c962e69201ae287a9d4270a946fe87b805e53
GaneshMBabu <mganeshbabur@gmail.com>
2017-06-22 10:20:30 +0530

    Update check_even_odd.py

--- check_even_odd.py
+++ check_even_odd.py
@@ -1,8 +1,11 @@
 n = int(input("Enter the number to check: "))
-def check(a):
-       num=a
-       if (num % 2 == 0):
-              print("even")
-       else :
-               print ("odd")
-check(n)
+if (n > 0):
+       def check(a):
+              num=a
+              if (num % 2 == 0):
+                     print("even")
+              else :
+                     print ("odd")
+       check(n)
+else:
+       print("Enter a Valid input")
